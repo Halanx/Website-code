11a28452c74f416189592625bb3c70643e8e2ced
Aakash <1998aakashrastogi@gmail.com>
2018-08-01 03:08:37 +0530

    changes done

--- scripts/controllers/store.js
+++ scripts/controllers/store.js
@@ -8,40 +8,41 @@
  * Controller of the halanxApp
  */
 angular.module('halanxApp')
-  .controller('StoreCtrl', function ($scope,store,$window) {
+  .controller('StoreCtrl', function ($scope, $rootScope, store, $window) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-     $scope.message = "";
-    $scope.submitform = ()=>{
-    
-        var obj = {
-          "FirstName": $scope.firstname,
-          "LastName":$scope.lastname,
-          "dealer-designation":$scope.desig,
-          "email":$scope.email,
-          "password":$scope.password,
-          "dealer-contact-number": $scope.mobilenumber
-        };
+    $rootScope.isMerchantPage = true;
+    $scope.message = "";
+    $scope.submitform = () => {
 
-        // obj.FirstName = $scope.firstname;
-        // obj.LastName = $scope.lastname;
-        // obj.dealer = $scope.storename;
-        // // obj.StoreAddress = $scope.storeaddress;
-        // obj.email=$scope.email
-        // obj.password = $scope.password;
-         
-        console.log(obj)
-       
-          var promise = store.callserver(obj);
-          promise.then((data)=>{
-            $scope.message = "Successfully registered, Halanx will contact you within next 2-3 business days."
-				console.log(data);
-			},(err)=>{
-				//  $scope.message = "Please Try Again!";
-			})
-       
+      var obj = {
+        "FirstName": $scope.firstname,
+        "LastName": $scope.lastname,
+        "dealer-designation": $scope.desig,
+        "email": $scope.email,
+        "password": $scope.password,
+        "dealer-contact-number": $scope.mobilenumber
+      };
+
+      // obj.FirstName = $scope.firstname;
+      // obj.LastName = $scope.lastname;
+      // obj.dealer = $scope.storename;
+      // // obj.StoreAddress = $scope.storeaddress;
+      // obj.email=$scope.email
+      // obj.password = $scope.password;
+
+      console.log(obj)
+
+      var promise = store.callserver(obj);
+      promise.then((data) => {
+        $scope.message = "Successfully registered, Halanx will contact you within next 2-3 business days."
+        console.log(data);
+      }, (err) => {
+        //  $scope.message = "Please Try Again!";
+      })
+
     }
   });
