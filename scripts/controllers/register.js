e607219f4aa4018ed3e1fa5e24c1def8e8aa82d8
Aakash <1998aakashrastogi.com>
2019-02-15 10:59:57 +0530

    update api

--- scripts/controllers/register.js
+++ scripts/controllers/register.js
@@ -8,14 +8,14 @@
  * Controller of the halanxApp
  */
 angular.module('halanxApp')
-  .controller('RegisterCtrl', function ($scope,register,$window) {
+  .controller('RegisterCtrl', function ($scope, register, $window) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-    if(localStorage.getItem("isLogin") ==true){
-     $window.location.href = "#stores";
+    if (localStorage.getItem("isLogin") == true) {
+      $window.location.href = "#stores";
     }
     $scope.firstnameError = false;
     $scope.lastnameError = false;
@@ -28,74 +28,73 @@ angular.module('halanxApp')
     $scope.email = "";
     $scope.password = "";
     $scope.mobilenumber = "";
-    
-    $scope.registerVerify = ()=> {
-      if($scope.firstname.length==0) {
+
+    $scope.registerVerify = () => {
+      if ($scope.firstname.length == 0) {
         $scope.firstnameError = true;
       } else {
         $scope.firstnameError = false;
       }
-      if($scope.lastname.length==0) {
-       $scope.lastnameError = true;
-     }else {
-      $scope.lastnameError = false;
-    }
-     if($scope.mobilenumber.length!=10) {
-       $scope.mobileError = true;
-     }else {
-      $scope.mobileError = false;
-    }
-     if($scope.password.length==0) {
-       $scope.passwordError = true;
-     }else {
-      $scope.passwordError = false;
-    }
-     if($scope.email.length==0) {
-       $scope.emailError = true;
-     }else {
-      $scope.emailError = false;
-    }
+      if ($scope.lastname.length == 0) {
+        $scope.lastnameError = true;
+      } else {
+        $scope.lastnameError = false;
+      }
+      if ($scope.mobilenumber.length != 10) {
+        $scope.mobileError = true;
+      } else {
+        $scope.mobileError = false;
+      }
+      if ($scope.password.length == 0) {
+        $scope.passwordError = true;
+      } else {
+        $scope.passwordError = false;
+      }
+      if ($scope.email.length == 0) {
+        $scope.emailError = true;
+      } else {
+        $scope.emailError = false;
+      }
     }
 
-    $scope.submitform = ()=>{
+    $scope.submitform = () => {
       $scope.registerVerify();
 
-        if($scope.firstname.length!=0 && $scope.lastname.length!=0 && $scope.email.length!=0 && $scope.password.length!=0 && $scope.mobilenumber.length==10) {
+      if ($scope.firstname.length != 0 && $scope.lastname.length != 0 && $scope.email.length != 0 && $scope.password.length != 0 && $scope.mobilenumber.length == 10) {
         var obj = {};
         console.log(localStorage.FirstName);
         obj.FirstName = $scope.firstname;
-        obj.PhoneNo=$scope.mobilenumber;
-        
-        var FirstName= $scope.firstname;
-        localStorage.setItem('FirstName',FirstName);
-        var LastName= $scope.lastname;
-        localStorage.setItem('LastName',LastName);
-        var PhoneNo= $scope.mobilenumber;
-        localStorage.setItem('PhoneNo',PhoneNo);
-        var email=$scope.email;
-        localStorage.setItem('email',email);
-        var password=$scope.password;
-        localStorage.setItem('password',password);
+        obj.PhoneNo = $scope.mobilenumber;
+
+        var FirstName = $scope.firstname;
+        localStorage.setItem('FirstName', FirstName);
+        var LastName = $scope.lastname;
+        localStorage.setItem('LastName', LastName);
+        var PhoneNo = $scope.mobilenumber;
+        localStorage.setItem('PhoneNo', PhoneNo);
+        var email = $scope.email;
+        localStorage.setItem('email', email);
+        var password = $scope.password;
+        localStorage.setItem('password', password);
 
 
-         
+
         console.log(obj)
-        
-          var promise = register.callserver(obj);
-          promise.then((data)=>
-          {
+
+        var promise = register.callserver(obj);
+        promise.then((data) => {
           console.log(data);
           // $window.location.href="http://localhost/test2/halanx-final/new1big-kfc/frontpage/registerotp.html";
           $window.location.assign("#registerotp")
-                   
 
-                 
-        
-           
-          },(err)=>{
-            $scope.registerError = true;
-          })
-       
+
+
+
+
+        }, (err) => {
+          $scope.registerError = true;
+        })
+
+      }
     }
-  }
   });
--- scripts/services/registerotp.js
+++ scripts/services/registerotp.js
@@ -8,28 +8,28 @@
  * Factory in the halanxApp.
  */
 angular.module('halanxApp')
-  .factory('registerotp', function ($http,$q) {
-        var object =  {
-		
-		
-        callserver : function(obj){
-            console.log(obj)
-            var pr = $q.defer();
-				var url = "https://api.halanx.com/users/";
-            console.log(obj);
-				$http.post(url,obj).then(function(data){
-					pr.resolve(data.data);
-					 
-					console.log(data.data); 
-				}
-					,function(err){
-					pr.reject(err);	
-					console.log(" Error");
-					});
-            return pr.promise;
-				}
+	.factory('registerotp', function ($http, $q) {
+		var object = {
 
-			
+
+			callserver: function (obj) {
+				console.log(obj)
+				var pr = $q.defer();
+				var url = "https://api.halanx.com/users/register/";
+				console.log(obj);
+				$http.post(url, obj).then(function (data) {
+					pr.resolve(data.data);
+
+					console.log(data.data);
 				}
-    return object;
-  });
+					, function (err) {
+						pr.reject(err);
+						console.log(" Error");
+					});
+				return pr.promise;
+			}
+
+
+		}
+		return object;
+	});
